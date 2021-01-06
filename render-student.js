d779b1d08bf300940a2c7a79b745876180e78b2c
BRK97 <73640959+BRK97@users.noreply.github.com>
2021-01-06 19:19:56 +0200

    render student function (#22)

---
+++ render-student.js
@@ -0,0 +1,11 @@
+// sto2html = student object to html
+function renderStudent(student = {}) {
+
+    return `<div class="student">
+    <h4>${student.name}</h4>
+    <div class="photo"><img src='${student.photo}'>
+        </div>
+    <a href="${student.cvLink}">link to CV</a>
+    </div>`;
+
+}
