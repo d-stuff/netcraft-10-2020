10136e396ef7f363398fa7198a4ce87342ac502c
barak <bo7991@gmail.com>
2021-01-06 19:16:33 +0200

    update

--- render-student.js
+++ render-student.js
@@ -1,12 +1,13 @@
 // sto2html = student object to html
 function renderStudent(student = {}) {
-    let students_html = "";
+
+    let student_html = "";
 
     student_html += `<div class="student">
-    <h4>${student_list[i].name}</h4>
-    <div class="photo"><img src='${student_list[i].photo}'>
+    <h4>${student_list.name}</h4>
+    <div class="photo"><img src='${student_list.photo}'>
         </div>
-    <a href="${student_list[i].cvLink}">link to CV</a>
+    <a href="${student_list.cvLink}">link to CV</a>
     </div>`
 
     return student_html;
