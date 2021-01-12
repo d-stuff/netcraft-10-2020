2237ac3cc4d835641d209c96bec6073ebc73a57b
Ana <anaalamed.gmail.com>
2021-01-12 22:03:41 +0200

    removed unnecessary data render function

--- render-student.js
+++ render-student.js
@@ -1,26 +1,14 @@
-function renderData(students) {
-    for (let student of students) {
-        if (student.photo === null) {
-            student.photo = "https://oldough.com/static/images/avatar.png";
-        }
-        if (student.bio === null) {
-            student.bio = "This is my bio...";
-        }
-    }
-}
-
 // sto2html = student object to html
 function renderStudent(student = {}) {
 	return `<div class="student">
     <h4>${student.name}</h4>
-    <div class="photo"><img src='${student.photo}'>
+    <div class="photo"><img src='${student.photo || "https://oldough.com/static/images/avatar.png"}'>
         </div>
-    <div class="bio">${student.bio}</div>
+    <div class="bio">${student.bio || "This is my bio..."}</div>
     <a href="${student.cvLink}">link to CV</a>
     </div>`;
 }
 
 export function renderStudents(students = []) {
-    renderData(students);
 	document.querySelector('.students').innerHTML = students.map(renderStudent).join("")
 }
