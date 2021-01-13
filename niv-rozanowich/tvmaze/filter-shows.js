c027d0d3c7a4939c76db8a7c6d1472c6c68c682b
Nivo <71404920+NivoDev@users.noreply.github.com>
2021-01-13 18:10:49 +0200

    Real search via tvmaze shows (#30)

--- niv-rozanowich/tvmaze/filter-shows.js
+++ niv-rozanowich/tvmaze/filter-shows.js
@@ -1,5 +1,6 @@
 export function filterShows(shows, word = '') {
 	const lowerWord = word.toLowerCase();
+
 	return shows.filter(function ({ show: { name, summary } }) {
 		return (
 			(typeof name === 'string' && name.toLowerCase().includes(lowerWord)) ||
